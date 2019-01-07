f8345a069cff500982c5bda93679d919df5200a9
zhl <390626411@qq.com>
2019-01-07 20:30:29 +0800

    商品页面前的都完成了

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -57,6 +57,7 @@ $(function(){
             dataType:"json",
             success:function(info){
 
+                console.log(info);
                 var htmlStr = template("selectTpl" , info);
 
                 $(".dropdown-menu").html(htmlStr);
@@ -70,7 +71,15 @@ $(function(){
         var txt = $(this).text();
 
         $("#dropdown-text").text(txt);
-    })
+
+        var id = $(this).data("id");
+
+        console.log(id);
+
+        $('[name="categoryId"]').val(id);
+
+        $("#form").data("bootstrapValidator").updateStatus("categoryId", "VALID")
+    })  
 
     // 文件
     $("#fileupload").fileupload({
@@ -79,7 +88,81 @@ $(function(){
             console.log(data);
             var picUrl = data.result.picAddr;
             $("#imgbox img").attr("src" , picUrl);
+
+            $('[name="brandLogo"]').val(picUrl);
+
+            $("#form").data("bootstrapValidator").updateStatus("brandLogo", "VALID")
         }
+    })
+
+    //表单校验
+    $("#form").bootstrapValidator({
+
+        excluded: [],
+
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+
+        fields:{
+            categoryId:{
+                validators:{
+                    notEmpty:{
+                        message:"请选择一级分类"
+                    }
+                }
+            },
+
+            brandName:{
+                validators:{
+                    notEmpty:{
+                        message:"请选择二级分类"
+                    }
+                }
+            },
+
+            brandLogo:{
+                validators:{
+                    notEmpty:{
+                        message:"请传入图片"
+                    }
+                }
+            }
+        }
+    })
+
+
+    // 上传内容
+    $("#form").on("success.form.bv" , function(e){
+
+        e.preventDefault();
+
+        $.ajax({
+
+            type:"post",
+            url:"/category/addSecondCategory",
+            data:$("#form").serialize(),
+            dataType:"json",
+            success:function(info){
+
+                console.log(info);
+
+                if(info.success){
+
+                    $("#secondModal").modal("hide");
+
+                    currentPage=1;
+                    render();
+
+                    $("#form").data("bootstrapValidator").resetForm(true);
+                    $("#dropdown-text").text("请选择一级分类");
+                    $("#imgbox img").attr("src" , "../images/none.png");
+                }
+            }
+        })
+
     })
 
 })
